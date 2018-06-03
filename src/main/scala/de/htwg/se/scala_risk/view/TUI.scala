5375221788d13736144217df8eaeeb51c82d537e
MarkusBuechler <markus.buechler@mail.de>
2018-06-03 14:04:34 +0200

    first method

--- src/main/scala/de/htwg/se/scala_risk/view/TUI.scala
+++ src/main/scala/de/htwg/se/scala_risk/view/TUI.scala
@@ -4,12 +4,78 @@ import de.htwg.se.scala_risk.controller.GameLogic
 import de.htwg.se.scala_risk.util.observer.TObserver
 import de.htwg.se.scala_risk.util.Statuses
 import javax.inject.Inject
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.actor.ActorSystem
+import akka.http.scaladsl.server.StandardRoute
+import akka.stream.ActorMaterializer
 import org.apache.log4j._
 
-class TUI @Inject() (gameLogic: GameLogic) extends TObserver {
+class TUI @Inject() (gameLogic: GameLogic) {
   val LENGTH = 30
 
-  gameLogic.add(this)
+  //gameLogic.add(this)
+
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+
+  val route =
+    get {
+      path("status") {
+        this.gameLogic.getStatus match {
+          case Statuses.INITIALIZE_PLAYERS => printPlayerInitialisation
+            /*
+          case Statuses.GAME_INITIALIZED => printPitch
+          case Statuses.PLAYER_SPREAD_TROOPS => printSpreadTroops
+          case Statuses.PLAYER_ATTACK => printAttack
+          case Statuses.PLAYER_MOVE_TROOPS => printMoveTroopss
+          case Statuses.DIECES_ROLLED => printRolledDieces
+          case Statuses.PLAYER_CONQUERED_A_COUNTRY => printConquered
+          case Statuses.PLAYER_CONQUERED_A_CONTINENT => logger.info("You conquered a continent!")
+          */
+          case _ => complete (HttpEntity (ContentTypes.`application/json`, "{\"status\":\"" + this.gameLogic.getStatus + "\"}") )
+        }
+      }
+    } ~
+      put {
+        path("start") {
+          this.gameLogic.startGame
+          val avColors = gameLogic.getAvailableColors.toString()
+          complete(HttpEntity(ContentTypes.`application/json`, "{\"status\":\"GAME_STARTED\", \"colors\":\"" + avColors.substring(5, avColors.length() - 1) + "\"}"))
+        } ~
+          path("initialize") {
+            this.gameLogic.initializeGame
+            complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, ""))
+          } ~
+          path("spread") {
+            formFields('country.as[String], 'amount.as[Int]) { (country: String, amount: Int) =>
+              this.gameLogic.addTroops(country, amount)
+              complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, ""))
+            }
+          } ~
+          path("attack") {
+            formFields('attacker.as[String], 'defender.as[String]) { (attacker: String, defender: String) =>
+              this.gameLogic.attack(attacker, defender)
+              val dices = gameLogic.getRolledDieces
+              complete(HttpEntity(ContentTypes.`application/json`,
+                "{\"dices\":[%s, %s]}".format("[" + dices._1.mkString(",") + "]", "[" + dices._2.mkString(",") + "]")))
+            }
+          } ~
+          path("endturn") {
+            this.gameLogic.endTurn
+            complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, ""))
+          }
+      } ~
+      post {
+        path("player") {
+          formFields('player.as[String], 'color.as[String]) { (player: String, color: String) =>
+            this.gameLogic.setPlayer((player, color))
+            complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, ""))
+          }
+        }
+      }
 
   val logger = Logger.getLogger(getClass.getName)
 
@@ -67,16 +133,19 @@ class TUI @Inject() (gameLogic: GameLogic) extends TObserver {
   def update() = {
     logger.debug("Status after update: " + gameLogic.getStatus)
     gameLogic.getStatus match {
-      case Statuses.INITIALIZE_PLAYERS => this.printPlayerInitialisation
-      case Statuses.GAME_INITIALIZED => printPitch
-      case Statuses.PLAYER_SPREAD_TROOPS => printSpreadTroops
-      case Statuses.PLAYER_ATTACK => printAttack
-      case Statuses.PLAYER_MOVE_TROOPS => printMoveTroops
-      case Statuses.DIECES_ROLLED => printRolledDieces
-      case Statuses.PLAYER_CONQUERED_A_COUNTRY => printConquered
-      case Statuses.PLAYER_CONQUERED_A_CONTINENT => logger.info("You conquered a continent!")
 
-      // Errors
+        /*
+        case Statuses.INITIALIZE_PLAYERS => this.printPlayerInitialisation
+        case Statuses.GAME_INITIALIZED => printPitch
+        case Statuses.PLAYER_SPREAD_TROOPS => printSpreadTroops
+        case Statuses.PLAYER_ATTACK => printAttack
+        case Statuses.PLAYER_MOVE_TROOPS => printMoveTroops
+        case Statuses.DIECES_ROLLED => printRolledDieces
+        case Statuses.PLAYER_CONQUERED_A_COUNTRY => printConquered
+        case Statuses.PLAYER_CONQUERED_A_CONTINENT => logger.info("You conquered a continent!")
+        */
+
+    // Errors
       case Statuses.COUNTRY_DOES_NOT_BELONG_TO_PLAYER => logger.error("COUNTRY_DOES_NOT_BELONG_TO_PLAYER")
       case Statuses.NOT_ENOUGH_TROOPS_TO_SPREAD => logger.error("NOT_ENOUGH_TROOPS_TO_SPREAD")
       case Statuses.COUNTRY_NOT_FOUND => logger.error("COUNTRY_NOT_FOUND")
@@ -200,11 +269,21 @@ class TUI @Inject() (gameLogic: GameLogic) extends TObserver {
     }
   }
 
+  /*
   private def printPlayerInitialisation = {
     logger.info("-----------------------------------------------------------------------");
     val avColors = gameLogic.getAvailableColors.toString()
     logger.info("Following colors are still available: " + avColors.substring(5, avColors.length() - 1));
     logger.info("Pleas enter v to start the game or a name and color which is available (name, color) to create a player:");
+  } */
+
+  val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
+
+  private def printPlayerInitialisation: StandardRoute = {
+    val avColors = gameLogic.getAvailableColors.toString()
+    complete (HttpEntity (ContentTypes.`application/json`, "{\"-----------------------------------------------------------------------\n\""
+      + "Following colors are still available: " + avColors.substring(5, avColors.length() - 1) + "\n\""
+      + "Pleas enter v to start the game or a name and color which is available (name, color) to create a player:" + "\"}") )
   }
 
   private def printSpreadTroops = {
